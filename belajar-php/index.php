3c3515c3f543e628a2e44ebe463f728c5cd72b35
Mulanaking <mulanaramadhan.121@gmail.com>
2022-04-11 11:05:57 +0800

    belajar php

---
+++ belajar-php/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// disini letak kode kita nantinya
+$nama = 'SEAL';
+$umur = 21;
+
+echo "halo nama saya $nama";
+echo "<br>"
+echo "halo umur saya $umur";
+
+echo "<br>"
+
+// define nama dan mas akmal
+
+$nama = "nazif"
+
+echo "halo nama saya". $nama;
+
+$nama = array("")
+?>
